0a3958b4b7d7b2e33e735d8a11af22172ccfd8f1
fedi <100169054+ta7fou@users.noreply.github.com>
2022-03-24 19:56:46 +0100

    Update enigme.c

--- enigme.c
+++ enigme.c
@@ -1,108 +1,290 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "SDL/SDL.h"
-#include "SDL/SDL_image.h"
-#include "SDL/SDL_mixer.h"
-#include "SDL/SDL_ttf.h"
 #include "enigme.h"
-#include <time.h>
-enigme initEnigme (){
-  enigme e;
-  e.f = fopen("lesenigmes.txt", "r");
- e.back = IMG_Load("letter.jpg");
- return e;
+
+
+enigme generer(){
+    enigme e;
+    int sol,ajout;
+    int i;
+
+    int coeffQ[4];
+    srand( time( NULL ) );
+    coeffQ[0]=rand() % 30;
+    coeffQ[1]=coeffQ[0]+12;
+    coeffQ[2]=coeffQ[1]-5;
+    coeffQ[3]=coeffQ[0]+coeffQ[2];
+
+
+
+	sprintf(e.question,"(%d * %d)+ %d - %d =?",coeffQ[0],coeffQ[1],coeffQ[2],coeffQ[3]);
+ 
+    sol=(coeffQ[0]*coeffQ[1])+coeffQ[2]-coeffQ[3];//calculer la sol
+    
+    
+    
+
+    srand( time( NULL ) );
+    e.sol =1 + rand() % 3;
+    if (e.sol==1)
+    {
+        sprintf(e.rep1,"%d",sol);
+        srand( time( NULL ) );
+        ajout =rand() % 15;
+        sprintf(e.rep2,"%d",sol+ajout);
+        srand( time( NULL ) );
+        ajout =(rand() % 15)-20;
+        sprintf(e.rep3,"%d",sol+ajout);
+    }
+    if (e.sol==2)
+    {
+        sprintf(e.rep2,"%d",sol);
+        srand( time( NULL ) );
+        ajout =rand() % 15;
+        sprintf(e.rep1,"%d",sol+ajout);
+        srand( time( NULL ) );
+        ajout =(rand() % 15)-20;
+        sprintf(e.rep3,"%d",sol+ajout);
+    }
+    if (e.sol==3)
+    {
+        sprintf(e.rep3,"%d",sol);
+        srand( time( NULL ) );
+        ajout =rand() % 15;
+        sprintf(e.rep1,"%d",sol+ajout);
+        srand( time( NULL ) );
+        ajout =(rand() % 15)-20;
+        sprintf(e.rep2,"%d",sol+ajout);
+    }
+    return e;
+    
+    
+
 }
-void showEnigme (enigmeData ed, SDL_Surface *screen, enigme e){
-  typedef struct{
-    SDL_Rect back;
-    SDL_Rect quest;
-    SDL_Rect c1;
-    SDL_Rect c2;
-    SDL_Rect c3;
-  }posenigme;
-posenigme pe;
-SDL_Surface *questsurf;
-SDL_Surface *c1surf;
-SDL_Surface *c2surf;
-SDL_Surface *c3surf;
-TTF_Font * police;
-SDL_Color red = {255,0,0};
-SDL_Color white = {0,0,0};
-if (TTF_Init() < 0) {
-  printf("error\n");
-}else{
-police = TTF_OpenFont("Bebas-Regular.ttf", 24);
-if (police == NULL) {
-  printf("error dans open font enigma\n");
-}else{
-  questsurf = TTF_RenderText_Solid(police, ed.quest, white);
-  c1surf = TTF_RenderText_Solid(police, ed.c1, red);
-  c2surf = TTF_RenderText_Solid(police, ed.c2, red);
-  c3surf = TTF_RenderText_Solid(police, ed.c3, red);
-  pe.back.x = 200;
-  pe.back.y = 100;
-  pe.back.h = questsurf->h;
-  pe.back.w = questsurf->w;
-  pe.quest.x = 240;
-  pe.quest.y = 130;
-  pe.c1.x= 400;
-  pe.c1.y= 200;
-  pe.c2.x= 550;
-  pe.c2.y= 200;
-  pe.c3.x= 700;
-  pe.c3.y= 200;
-  SDL_BlitSurface(e.back, NULL, screen, &pe.back);
-  SDL_BlitSurface(questsurf, NULL, screen, &pe.quest);
-  SDL_BlitSurface(c1surf, NULL, screen, &pe.c1);
-  SDL_BlitSurface(c2surf, NULL, screen, &pe.c2);
-  SDL_BlitSurface(c3surf, NULL, screen, &pe.c3);
-TTF_CloseFont(police);
-TTF_Quit();
+void afficherEnigme(enigme *e, SDL_Surface * screen){
+      
+SDL_Surface *texte,*R1,*R2,*R3,*back,*bouton;
+SDL_Rect posQ,pos1,pos2,pos3,posbouton1,posbouton2,posbouton3;
+SDL_Color blanc = {255, 255, 255};
+TTF_Font *police = NULL,*police2 = NULL;
+
+bouton=IMG_Load("bouton.png");
+back=IMG_Load("backenig.png");
+
+
+TTF_Init();
+       
+police = TTF_OpenFont("clerk.ttf", 90);
+police2 = TTF_OpenFont("pepsi.ttf", 90);
+
+
+    texte = TTF_RenderText_Blended(police, e->question, blanc);
+    R1 = TTF_RenderText_Blended(police2, e->rep1, blanc);
+    R2 = TTF_RenderText_Blended(police2, e->rep2, blanc);
+    R3 = TTF_RenderText_Blended(police2, e->rep3, blanc);
+
+
+
+    posQ.x=170;
+    posQ.y=80;
+    pos1.x=100;
+    pos1.y=420;
+    posbouton1.x=30;
+    posbouton1.y=400;
+    pos2.x=520;
+    pos2.y=420;
+    posbouton2.x=450;
+    posbouton2.y=400;
+    pos3.x=980;
+    pos3.y=420;
+    posbouton3.x=900;
+    posbouton3.y=400;
+
+    SDL_BlitSurface(back, NULL, screen, NULL); 
+    SDL_BlitSurface(texte, NULL, screen, &posQ);
+    SDL_BlitSurface(bouton,NULL,screen,&posbouton1);
+    SDL_BlitSurface(bouton,NULL,screen,&posbouton2);
+    SDL_BlitSurface(bouton,NULL,screen,&posbouton3);
+    SDL_BlitSurface(R1, NULL, screen, &pos1); 
+    SDL_BlitSurface(R2, NULL, screen, &pos2); 
+    SDL_BlitSurface(R3, NULL, screen, &pos3); 
 }
-}
-}
-int randomize(){
-int pos;
- srand(time(NULL));
-pos=rand()%(9-0+1)+0;
-return pos;
-}
-enigmeData generateEnigme(enigme e){
-  enigmeData ed [10];
-  enigmeData edata;
-  int i;
-  int pos = randomize();
-  for (i = 0; i < 10; i++) {
-    fgets(ed[i].quest, 400, e.f);
-    fscanf(e.f,"1)%s 2)%s 3)%s -%s\n",ed[i].c1, ed[i].c2, ed[i].c3, ed[i].x);
-  }
-  strcpy(edata.quest,ed[pos].quest);
-  strcpy (edata.c1 ,ed[pos].c1);
-      strcpy(edata.c2 ,ed[pos].c2);
-        strcpy(edata.c3 ,ed[pos].c3);
-          strcpy(edata.x ,ed[pos].x);
-  return edata;
-}
-int resolutionEnigme(enigmeData ed, SDL_Event event){
-  switch(event.type){
-         case SDL_KEYDOWN:
-            if(event.key.keysym.sym == SDLK_1){
-             if (strcmp (ed.c1, ed.x) == 0) {
-               return 1;
-             }
-            }else{
-              if(event.key.keysym.sym == SDLK_2){
-                if (strcmp (ed.c2, ed.x) == 0) {
-                  return 1;
+int enigme_alea()
+{
+int resultat;
+SDL_Init(SDL_INIT_VIDEO);
+SDL_Surface *screen=NULL;
+SDL_Surface *back=NULL;
+SDL_Surface *winning=NULL;
+SDL_Surface *losing=NULL;
+winning=IMG_Load("winning.png");
+losing=IMG_Load("losing.png");
+screen=SDL_SetVideoMode (1200,600,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+SDL_Event event;
+SDL_WM_SetCaption("Enigme Anas\t1",NULL);
+bool execute=true;
+int reponse=-1;
+enigme e;
+e=generer();
+while (execute)
+{
+    afficherEnigme(&e,screen);
+    SDL_Flip(screen);
+    while(SDL_PollEvent(&event))
+	{
+
+		switch(event.type)
+		{
+		case SDL_QUIT:
+
+			execute=false;
+		break;
+        case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			
+			case SDLK_a :
+				if (e.sol==1)
+                {
+                    reponse=1;
+                    printf("vrai");
+                    SDL_BlitSurface(winning, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(6000);
+                    resultat=1;
+                    execute=0;
                 }
-             }else{
-               if(event.key.keysym.sym == SDLK_3){
-                 if (strcmp (ed.c3, ed.x) == 0) {
-                   return 1;
-                 }
-               }
-             }
+                else {
+                    reponse=0;
+                    printf("faux");
+                    SDL_BlitSurface(losing, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=0;
+                    execute=0;
+                }
+                
+      		break;
+            case SDLK_z :
+                if (e.sol==2)
+                {
+                    printf("vrai");
+                    reponse=1;
+                    SDL_BlitSurface(winning, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=1;
+                    execute=0;
+                }
+                else {
+                    printf("faux");
+                    reponse=0;
+                    SDL_BlitSurface(losing, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=0;
+                    execute=0;
+                }
+            break;
+            case SDLK_e :
+                if (e.sol==3)
+                {
+                    printf("vrai");
+                    reponse=1;
+                    SDL_BlitSurface(winning, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=1;
+                    execute=0;
+                }
+                else {
+                   reponse=0;
+                    printf("faux");
+                    SDL_BlitSurface(losing, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=0;
+                    execute=0;
+                }
+            break;
             }
+        break;
+        case SDL_MOUSEBUTTONDOWN:
+            if (event.motion.x >= 30 && event.motion.x <= 303 && event.motion.y >= 420 && event.motion.y <= 558)
+            {
+                if (e.sol==1)
+                {
+                    printf("vrai");
+                    reponse=1;
+                    printf("vrai");
+                    SDL_BlitSurface(winning, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(6000);
+                    resultat=1;
+                    execute=0;
+      
+                }
+                else {
+                    printf("faux");
+                    reponse=0;
+                    SDL_BlitSurface(losing, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=0;
+                    execute=0;
+                }
+            }
+            if (event.motion.x >= 450 && event.motion.x <= 723 && event.motion.y >= 400 && event.motion.y <= 573)
+            {
+                if (e.sol==2)
+                {
+                    printf("vrai");
+                    reponse=1;
+                    printf("vrai");
+                    SDL_BlitSurface(winning, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(6000);
+                    resultat=1;
+                    execute=0;
+                }
+                else {
+                    printf("faux");
+                    reponse=0;
+                    SDL_BlitSurface(losing, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=0;
+                    execute=0;
+                }
+            }
+            if (event.motion.x >= 980 && event.motion.x <= 1253 && event.motion.y >= 400 && event.motion.y <= 573)
+            {
+                if (e.sol==3)
+                {
+                    printf("vrai");
+                    reponse=1;
+                    printf("vrai");
+                    SDL_BlitSurface(winning, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(6000);
+                    resultat=1;
+                    execute=0;
+                }
+                else {
+                    printf("faux");
+                    reponse=0;
+                    SDL_BlitSurface(losing, NULL, screen, NULL);
+                    SDL_Flip(screen);
+                    SDL_Delay(4000);
+                    resultat=0;
+                    execute=0;
+                }
+            }
+            break;
+        }
+    }
 }
-return 0;
+
+
+
+return resultat;
 }
+
